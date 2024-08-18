88780405c86351a2225e276cf66ebf47dce126a7
Min Sang Kim <129925473+MinSang22Kim@users.noreply.github.com>
2024-08-18 20:00:05 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void){
+    int n;
+    scanf("%d", &n);
+    if(n==0)
+        printf("YONSEI\n");
+    else
+        printf("Leading the Way to the Future\n");
+    return 0;
+}
