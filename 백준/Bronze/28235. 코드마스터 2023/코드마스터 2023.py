955f7b6cd2c32535601e7d95b803d235ad04dcf5
Min Sang Kim <129925473+MinSang22Kim@users.noreply.github.com>
2024-08-19 01:15:51 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+yell = input()
+
+if yell == "SONGDO":
+    print("HIGHSCHOOL")
+elif yell == "CODE":
+    print("MASTER")
+elif yell == "2023":
+    print("0611")
+elif yell == "ALGORITHM":
+    print("CONTEST")
