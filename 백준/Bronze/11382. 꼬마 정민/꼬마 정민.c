fcc9d2bde16dbb5aae11ac1118af733f0deadf52
Min Sang Kim <129925473+MinSang22Kim@users.noreply.github.com>
2024-08-18 19:55:14 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void){
+    long long a, b, c;
+    scanf("%lld %lld %lld", &a, &b, &c);
+    printf("%lld\n", a+b+c);
+    return 0;
+}
