b174cb5f39f47adb8b9b89b5841353ae10418557
Min Sang Kim <129925473+MinSang22Kim@users.noreply.github.com>
2024-08-19 00:18:41 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static int gcd(int a, int b) {
+		while (b != 0) {
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
+	} // gcd(a,b) = gcd(b,r)...
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+
+		int d = gcd(a, b); // 최대공약수
+		System.out.println(d);
+		System.out.println(a * b / d); // 최대공배수
+
+		scan.close();
+	}
+}
