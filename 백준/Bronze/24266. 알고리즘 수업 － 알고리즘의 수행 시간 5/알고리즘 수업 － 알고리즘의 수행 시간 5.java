5ae4750f3aa43d4b7494b6afaf6930fcfe0e08fe
Min Sang Kim <129925473+MinSang22Kim@users.noreply.github.com>
2024-09-05 12:14:39 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -2,7 +2,6 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         long n = Integer.parseInt(br.readLine());
@@ -10,7 +9,6 @@ public class Main {
     }
 
     private static void checkTimeComplexity(long n) {
-
         System.out.println(n*n*n);
         System.out.print(3);
     }
