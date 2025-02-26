cd76b2ac19aa0a1f7a8e1373d155800827475af8
Min Sang Kim <202014889@kyonggi.ac.kr>
2025-02-26 15:23:24 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] numList) {
         int[] answer = new int[numList.length];
@@ -7,6 +5,7 @@ class Solution {
         for(int i = 0; i < numList.length; i++){
             answer[i] = numList[numList.length - i - 1];
         }
+        
         return answer;
     }
 }
