96cc46b478c2e641f1415c7d515f915912a457ca
Min Sang Kim <202014889@kyonggi.ac.kr>
2025-03-02 20:24:25 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,12 +1,11 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] numList) {
         int[] arr = new int[2];
+        
         for(int i = 0; i < numList.length; i++){
-            if(numList[i] % 2 == 0) arr[0]++;
-            else arr[1]++;
+            arr[numList[i] % 2]++;
         }
+        
         return arr;
     }
 }
