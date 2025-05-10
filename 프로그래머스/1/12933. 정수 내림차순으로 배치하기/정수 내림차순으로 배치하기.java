0a74c815661328693c75c0fbe765bed0adbb15b0
김민상 <202014889@kyonggi.ac.kr>
2025-05-10 16:44:10 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.57 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public long solution(long n) {
+        char[] arr = Long.toString(n).toCharArray();
+        Arrays.sort(arr);
+        
+        StringBuilder sb = new StringBuilder(new String(arr));
+        sb.reverse();
+        
+        return Long.parseLong(sb.toString());
+    }
+}
