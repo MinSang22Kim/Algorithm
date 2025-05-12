9ab5d3cd521d03fdc4cab6f405a3804f9412bb76
김민상 <202014889@kyonggi.ac.kr>
2025-05-12 15:26:47 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 12.17 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,14 +1,10 @@
 class Solution {
     public String solution(String[] seoul) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("김서방은 ");
         for(int i = 0; i < seoul.length; i++){
             if(seoul[i].equals("Kim")){
-                sb.append(i);
-                break;
+                return "김서방은 " + i + "에 있다";
             }
         }
-        sb.append("에 있다");
-        return sb.toString();
+        return null;
     }
 }
