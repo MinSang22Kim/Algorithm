a0b62f9f3b0c5e23e08822e0db2f1e5d45b82be3
김민상 <202014889@kyonggi.ac.kr>
2025-05-13 14:01:06 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.06 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -5,7 +5,6 @@ class Solution {
         long sqrt = (long)Math.sqrt(n);
         if(Math.pow(sqrt, 2) == n)
             return (long)Math.pow(++sqrt, 2);
-        else
-            return -1;
+        return -1;
     }
 }
