556415725b8f9e762a97bc64c3f869a77f88503d
김민상 <202014889@kyonggi.ac.kr>
2025-05-15 17:39:30 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean solution(int x) {
+        int originX = x;
+        int sum = 0;
+        while(x != 0){
+            sum += x % 10;
+            x /= 10;
+        }
+        if(originX % sum == 0) return true;
+        return false;
+    }
+}
