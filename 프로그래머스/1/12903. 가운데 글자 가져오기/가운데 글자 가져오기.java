f6a037a0114dde06a66a4b3f4b3e67ce5d5d5d2a
김민상 <202014889@kyonggi.ac.kr>
2025-05-20 18:56:45 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -2,7 +2,7 @@ class Solution {
     public String solution(String s) {
         int len = s.length();
         return (len % 2 == 0) 
-            ? String.valueOf(s.charAt(len/2 - 1) + "" + s.charAt(len/2)) 
+            ? String.valueOf(s.substring(len / 2 - 1 , len / 2 + 1)) 
             : String.valueOf(s.charAt(len/2));
     }
 }
