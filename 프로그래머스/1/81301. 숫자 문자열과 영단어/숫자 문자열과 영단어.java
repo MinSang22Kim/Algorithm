31f82bc70d805bd356f33232028e4219bda3ad10
김민상 <202014889@kyonggi.ac.kr>
2025-05-21 16:22:41 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.58 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,30 +1,10 @@
-import java.util.Random;
-
 class Solution {
     public int solution(String s) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
-            if(Character.isDigit(s.charAt(i))){
-                sb.append(s.charAt(i));
-            }
-            else {
-                if (i + 1 >= s.length()) break;
-                String sub = s.substring(i, i+2);
-                switch(sub){
-                    case "ze": sb.append(0); i+=3; break;
-                    case "on": sb.append(1); i+=2; break;
-                    case "tw": sb.append(2); i+=2; break;
-                    case "th": sb.append(3); i+=4; break;
-                    case "fo": sb.append(4); i+=3; break;
-                    case "fi": sb.append(5); i+=3; break;
-                    case "si": sb.append(6); i+=2; break;
-                    case "se": sb.append(7); i+=4; break;
-                    case "ei": sb.append(8); i+=4; break;
-                    case "ni": sb.append(9); i+=3; break;
-                    default: break;
-                }
-            }
+        String[] words = {"zero", "one", "two","three", "four", 
+                          "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < words.length; i++){
+            s = s.replaceAll(words[i], String.valueOf(i));
         }
-        return Integer.parseInt(sb.toString());
+        return Integer.parseInt(s);
     }
 }
